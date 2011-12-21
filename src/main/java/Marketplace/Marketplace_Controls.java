eb6e7057106327a69b81cef9b02bc65d75733a25
Lee Chin Hwee <leec0107@e.ntu.edu.sg>
2011-12-21 09:25:00 +0800

    Added a new method to set for simulation analyzer

--- src/main/java/Marketplace/Marketplace_Controls.java
+++ src/main/java/Marketplace/Marketplace_Controls.java
@@ -7,15 +7,23 @@ import java.awt.*;
 public class Marketplace_Controls extends JPanel 
 {
 	Marketplace_Main market;
+	SimulationAnalyzer_Main outputReader;
 	//Button configurations
 	JButton marketBtns[] = new JButton[4];
 	String title[] = {"Import Configuration","Save Configuration","Reset","Run"};
 	
+	//For marketplace congfiguration
 	public Marketplace_Controls(Marketplace_Main market)
 	{
 		this.market = market;
 		this.setLayout(new FlowLayout(FlowLayout.LEFT));
 		initButtons();
+	}
+	
+	//For simulation analyzer
+	public void setSimAnalyzer(SimulationAnalyzer_Main outputReader)
+	{
+		this.outputReader = outputReader;
 	}
 	
 	//Initalise buttons
